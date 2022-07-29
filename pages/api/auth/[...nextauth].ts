f831655cbe64d3e2ff7443e11c434008d12a94fa
joonas <joonas.kaustel@gmail.com>
2022-07-29 18:27:31 +0300

    * fix login

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,50 +1,50 @@
-import NextAuth, {NextAuthOptions} from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import {supabase} from "../../../utils/supabaseClient";
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import { supabase } from '../../../utils/supabaseClient';
 
 export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-  ],
-  theme: {
-    colorScheme: "light",
-  },
-  callbacks: {
-    async session({ session, user, token  }) {
-      const { data: retrievedUser, error: getUserError } = await supabase
-        .from('user')
-        .select('*')
-        .filter('email', 'eq', session.user?.email);
-
-      // @ts-ignore
-      session.dbUserId = retrievedUser[0]['id'];
-      session.providerAccountId = token.sub;
-      return Promise.resolve(session);
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+        }),
+    ],
+    theme: {
+        colorScheme: 'light',
     },
-    async jwt({ token, user, account }) {
-      token.providerAccountId = account?.providerAccountId;
-      token.dbUserId = account?.dbUserId;
-      token.userRole = "admin"
-      return token
+    callbacks: {
+        async session({ session, user, token }) {
+            const { data: retrievedUser, error: getUserError } = await supabase
+                .from('user')
+                .select('*')
+                .filter('email', 'eq', session.user?.email);
+
+            // @ts-ignore
+            session.dbUserId = retrievedUser[0]['id'];
+            session.providerAccountId = token.sub;
+            return Promise.resolve(session);
+        },
+        async jwt({ token, user, account }) {
+            token.providerAccountId = account?.providerAccountId;
+            token.dbUserId = account?.dbUserId;
+            token.userRole = 'admin';
+            return token;
+        },
+        async signIn({ user, account, profile, email, credentials }) {
+            const { data: retrievedUser, error: getUserError } = await supabase
+                .from('user')
+                .select('*')
+                .filter('providerId', 'eq', user?.id);
+
+            if (!retrievedUser?.length) {
+                const { data: savedUser, error } = await supabase
+                    .from('user')
+                    .insert({ email: user.email, providerId: user.id });
+            }
+
+            return true;
+        },
     },
-    async signIn({ user, account, profile, email, credentials }) {
-      const { data: retrievedUser, error: getUserError } = await supabase
-        .from('user')
-        .select('*')
-        .filter('providerId', 'eq', user.id);
+};
 
-      if (!retrievedUser) {
-        const { data: savedUser, error } = await supabase
-          .from('user')
-          .insert({ email: user.email, providerId: user.id });
-      }
-
-      return true;
-    }
-  },
-}
-
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
