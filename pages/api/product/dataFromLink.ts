a8a989f64c818108fab97ca11750862addd6704a
joonas <joonas.kaustel@gmail.com>
2022-09-15 20:19:10 +0300

    * test puppeteer

--- pages/api/product/dataFromLink.ts
+++ pages/api/product/dataFromLink.ts
@@ -26,7 +26,9 @@ export default async function handler(
 
     if (!(siteHost as string in SiteEnums)) throw new Error(`SiteHost is invalid ${siteHost}`);
 
+    console.log('before browser')
     const browser = await puppeteer.launch();
+    console.log({browser})
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0')
     await page.goto(link as string);
